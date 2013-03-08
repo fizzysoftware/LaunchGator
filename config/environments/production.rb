96ab435119aaefcc8cb586ba0a35b6009ee9683b
Nikhil Kathuria <nikhilk@fizzysoftware.com>
2013-03-08 13:48:46 +0530

    minor changes

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -7,6 +7,7 @@ LaunchGator::Application.configure do
   # Full error reports are disabled and caching is turned on
   config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
+  config.action_mailer.default_url_options = { :host => 'launchgator.heroku.com' }
 
   # Disable Rails's static asset server (Apache or nginx will already do this)
   config.serve_static_assets = false
